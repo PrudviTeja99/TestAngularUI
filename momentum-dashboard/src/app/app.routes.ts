57e9c32bde6050bc135945a587c190c2b90f0ec2
PrudviTeja <prudvitejabandam987@gmail.com>
2024-02-17 15:25:02 +0530

    feat(routes): initial setup

--- momentum-dashboard/src/app/app.routes.ts
+++ momentum-dashboard/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ErrorComponent } from './pages/error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"login",component:LoginComponent},
+    {path:"dashboard",component:DashboardComponent},
+    {path:"",pathMatch:'full',redirectTo:"/dashboard"},
+    {path:'**',component:ErrorComponent}
+];
